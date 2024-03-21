34b32ed42ee1d3e8ba169fd6849ae6dafc177fca
sjjambhulkar@gmail.com <sjjambhulkar@gmail.com>
2024-03-21 11:32:05 +0000

    first commit

---
+++ src/test/java/service1/example/service1/Service1ApplicationTests.java
@@ -0,0 +1,13 @@
+package service1.example.service1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Service1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
